9aaf287c13ca2cdc016725f53c66fb354405c900
segundo rebaza <alvaro.rp1593@gmail.com>
2022-01-19 18:04:35 -0500

    faltan estilos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do
+  flat = Flat.new(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: Faker::Number.decimal_part,
+    number_of_guests: Faker::Number.within(range: 1..10)
+  )
+  flat.save!
+end
+puts 'finished!'
